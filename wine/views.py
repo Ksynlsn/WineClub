6e858519b3239707584ee007fda6438ce52e40a3
Ksynlsn <ksynlsn@gmail.com>
2022-05-23 16:49:25 -0700

    Adds wineview

--- wine/views.py
+++ wine/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Wine, Review, Country, Region
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
@@ -8,3 +9,7 @@ def index(request):
 def wines(request):
     wine_list=Wine.objects.all()
     return render(request, 'wine/wines.html', {'wine_list' : wine_list})
+
+def wineview(request, id):
+    wines=get_object_or_404(Wine, pk=id)
+    return render(request, 'wine/wineview.html', {'wines' : wines})
