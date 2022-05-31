edec877118d1028d5189c48c9d6a8cb4fe65dac4
Ksynlsn <ksynlsn@gmail.com>
2022-05-31 16:33:50 -0700

    Adds views for Regions and Reviews

--- wine/urls.py
+++ wine/urls.py
@@ -4,6 +4,12 @@ from . import views
 urlpatterns = [
    path('', views.index, name='index'),
    path('wine/', views.wines, name='wines'),
-   path('wineview/<int:id>', views.wineview, name='details'),
+   path('wineview/<int:id>', views.wineview, name='wine_details'),
+   path('regions/', views.regions, name='regions'),
+   path('regionview/<int:id>', views.regionview, name='region_details'),
+   path('reviews/', views.reviews, name='reviews'),
+   path('reviewview/<int:id>', views.reviewview, name='review_details'),
+ 
+   
    
 ]
--- wine/views.py
+++ wine/views.py
@@ -13,3 +13,19 @@ def wines(request):
 def wineview(request, id):
     wines=get_object_or_404(Wine, pk=id)
     return render(request, 'wine/wineview.html', {'wines' : wines})
+
+def regions(request):
+    region_list=Region.objects.all()
+    return render(request, 'wine/regions.html', {'region_list' : region_list})
+
+def regionview(request, id):
+    regions=get_object_or_404(Region, pk=id)
+    return render(request, 'wine/regionview.html', {'regions' : regions})
+
+def reviews(request):
+    review_list=Review.objects.all()
+    return render(request, 'wine/reviews.html', {'review_list' : review_list})
+
+def reviewview(request, id):
+    reviews=get_object_or_404(Review, pk=id)
+    return render(request, 'wine/reviewview.html', {'reviews' : reviews})
